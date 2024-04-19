61c057cc23c3f1d6a0291e239836afa4c9a3cb7d
Roy <pidlaoanralph0@gmail.com>
2024-04-18 19:59:40 -0600

    logout button added to home page

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,9 +1,24 @@
 import {useState} from 'react'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const Home = (props) => {
+    const navigate = useNavigate();
+
+    const logout = () => {
+        axios.post('http://localhost:8000/api/logout', {}, {withCredentials:true})
+            .then((res) => {
+                navigate('/')
+            })
+            .catch((err) => {
+                console.error(err);
+            })
+    }
+
     return (
         <div>
             <h2>Home</h2>
+            <button onClick={logout}>Logout</button>
         </div>
 )}
 
